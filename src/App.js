548465864a58d7d3d3fba8ce79aa56dd77dea16b
Jon Samp <sampjon@gmail.com>
2022-04-18 12:26:38 -0400

    updates react router to v 6

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { Exercise2 } from './pages/Exercise2';
 import { Exercise3 } from './pages/Exercise3';
@@ -7,18 +7,12 @@ import { Exercise4 } from './pages/Exercise4';
 function App() {
   return (
     <Router>
-      <Route path='/' exact>
-        <Home />
-      </Route>
-      <Route path='/exercises/2'>
-        <Exercise2 />
-      </Route>
-      <Route path='/exercises/3'>
-        <Exercise3 />
-      </Route>
-      <Route path='/exercises/4'>
-        <Exercise4 />
-      </Route>
+      <Routes>
+        <Route path='/' exact element={<Home />} />
+        <Route path='/exercises/2' exact element={<Exercise2 />} />
+        <Route path='/exercises/3' exact element={<Exercise3 />} />
+        <Route path='/exercises/4' exact element={<Exercise4 />} />
+      </Routes>
     </Router>
   );
 }
